460b6c746bb154ec2e9f1e8c69da4ecded571c3e
Tyler Gwin <tyler@gwins.net>
2019-11-20 17:04:24 -0500

    Need to Fix DefaultGridSampler

--- lib/common/DefaultGridSampler.dart
+++ lib/common/DefaultGridSampler.dart
@@ -41,31 +41,31 @@ class DefaultGridSampler extends GridSampler {
                               double p4FromX, double p4FromY, 
                               PerspectiveTransform transform}) {
 
-                                if(transform != null){
-                                    if (dimensionX <= 0 || dimensionY <= 0) {
-      throw NotFoundException.getNotFoundInstance();
+if(transform != null){
+    if (dimensionX <= 0 || dimensionY <= 0) {
+     throw Exception("Not Found Exception");
     }
     BitMatrix bits = new BitMatrix(dimensionX, dimensionY);
-    double[] points = new double[2 * dimensionX];
+    List<double> points = new List(2 * dimensionX);
     for (int y = 0; y < dimensionY; y++) {
       int max = points.length;
-      double iValue = y + 0.5f;
+      double iValue = y + 0.5;
       for (int x = 0; x < max; x += 2) {
-        points[x] = (double) (x / 2) + 0.5f;
+        points[x] =  (x / 2) + 0.5;
         points[x + 1] = iValue;
       }
-      transform.transformPoints(points);
+      transform.transformPoints(points: points);
       // Quick check to see if points transformed to something inside the image;
       // sufficient to check the endpoints
       checkAndNudgePoints(image, points);
       try {
         for (int x = 0; x < max; x += 2) {
-          if (image.get((int) points[x], (int) points[x + 1])) {
+          if (image.get(points[x], points[x + 1]) {
             // Black(-ish) pixel
             bits.set(x / 2, y);
           }
         }
-      } catch (ArrayIndexOutOfBoundsException aioobe) {
+      } catch (aioobe) {
         // This feels wrong, but, sometimes if the finder patterns are misidentified, the resulting
         // transform gets "twisted" such that it maps a straight line of points to a set of points
         // whose endpoints are in bounds, but others are not. There is probably some mathematical
@@ -73,61 +73,17 @@ class DefaultGridSampler extends GridSampler {
         // This results in an ugly runtime exception despite our clever checks above -- can't have
         // that. We could check each point's coordinates but that feels duplicative. We settle for
         // catching and wrapping ArrayIndexOutOfBoundsException.
-        throw NotFoundException.getNotFoundInstance();
+        throw Exception("Not Found Exception");
       }
     }
     return bits;
-                                }else{
-
-                                }
+  }else{                     
 
     PerspectiveTransform transform = PerspectiveTransform.quadrilateralToQuadrilateral(
         p1ToX, p1ToY, p2ToX, p2ToY, p3ToX, p3ToY, p4ToX, p4ToY,
         p1FromX, p1FromY, p2FromX, p2FromY, p3FromX, p3FromY, p4FromX, p4FromY);
 
-    return sampleGrid(image, dimensionX, dimensionY, transform=transform);
+    return sampleGrid(image, dimensionX, dimensionY, transform: transform);
   }
-
-  @Override
-  public BitMatrix sampleGrid(BitMatrix image,
-                              int dimensionX,
-                              int dimensionY,
-                              PerspectiveTransform transform) throws NotFoundException {
-    if (dimensionX <= 0 || dimensionY <= 0) {
-      throw NotFoundException.getNotFoundInstance();
-    }
-    BitMatrix bits = new BitMatrix(dimensionX, dimensionY);
-    double[] points = new double[2 * dimensionX];
-    for (int y = 0; y < dimensionY; y++) {
-      int max = points.length;
-      double iValue = y + 0.5f;
-      for (int x = 0; x < max; x += 2) {
-        points[x] = (double) (x / 2) + 0.5f;
-        points[x + 1] = iValue;
-      }
-      transform.transformPoints(points);
-      // Quick check to see if points transformed to something inside the image;
-      // sufficient to check the endpoints
-      checkAndNudgePoints(image, points);
-      try {
-        for (int x = 0; x < max; x += 2) {
-          if (image.get((int) points[x], (int) points[x + 1])) {
-            // Black(-ish) pixel
-            bits.set(x / 2, y);
-          }
-        }
-      } catch (ArrayIndexOutOfBoundsException aioobe) {
-        // This feels wrong, but, sometimes if the finder patterns are misidentified, the resulting
-        // transform gets "twisted" such that it maps a straight line of points to a set of points
-        // whose endpoints are in bounds, but others are not. There is probably some mathematical
-        // way to detect this about the transformation that I don't know yet.
-        // This results in an ugly runtime exception despite our clever checks above -- can't have
-        // that. We could check each point's coordinates but that feels duplicative. We settle for
-        // catching and wrapping ArrayIndexOutOfBoundsException.
-        throw NotFoundException.getNotFoundInstance();
-      }
-    }
-    return bits;
-  }
-
+}
 }
